0c9787bd8c8aaf7834c52779f1f5cfa2727953a6
Michael Scott <mike@foundries.io>
2018-08-13 16:11:34 -0700

    bt-joiner: update copyright
    Signed-off-by: Michael Scott <mike@foundries.io>

--- bt-joiner/bluetooth_6lowpand.sh
+++ bt-joiner/bluetooth_6lowpand.sh
@@ -2,6 +2,7 @@
 #
 # Copyright (c) 2017 Linaro Limited
 # Copyright (c) 2017 Open Source Foundries
+# Copyright (c) 2018 Foundries.io
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
