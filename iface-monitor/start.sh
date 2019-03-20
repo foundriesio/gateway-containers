a64248de7643f9f9e0c8a98cec4ca5ed673f1688
Michael Scott <mike@foundries.io>
2019-03-20 08:43:53 -0700

    iface-monitor: re-introduce stand-alone interface monitor container
    As a stand-alone container, we can easily configure multiple copies
    of iface-monitor based on the gateway needs, without having to
    worry about needing radvd services.
    
    NOTE: This was switched from a shell script to Python to eliminate
    the odd kill process hacks needed in the shell script.
    
    Signed-off-by: Michael Scott <mike@foundries.io>

---
+++ iface-monitor/start.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# Copyright (c) 2019 Foundries.io
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+MON_INTERFACE="bt0"
+MON_DYN_IP="fd11:11::1/64"
+
+function parse_args()
+{
+    while [ $# -gt 0 ]
+    do
+        case $1 in
+        --interface)
+            MON_INTERFACE=$2
+            shift
+            shift
+            ;;
+        --ip)
+            MON_DYN_IP=$2
+            shift
+            shift
+            ;;
+        *)
+            shift
+            ;;
+        esac
+    done
+}
+
+parse_args "$@"
+
+# Make dbus symlink
+rm -rf /run/dbus/
+ln -s /var/dbus /run/dbus
+
+# Run interface monitor
+# /interface-monitor.sh ${MON_INTERFACE} ${MON_DYN_IP}
+python2 /interface-monitor.py -i ${MON_INTERFACE} -d ${MON_DYN_IP}
